9036e4813aa1a441aa0a494ae5c8e8b1191e3b7d
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-01 15:06:22 +0700

    prosedur untuk menampilkan datanya

--- Struct1.cpp
+++ Struct1.cpp
@@ -15,4 +15,10 @@ int main(){
     cin >> mhs.nama;
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
 }
