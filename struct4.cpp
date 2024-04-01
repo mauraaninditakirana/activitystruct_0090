fdb890e2ee7e51774e104cd29c9aca88065cd2a5
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-01 15:44:31 +0700

    merubah codingan, karena salah lihat, mulai dari int main()

--- struct4.cpp
+++ struct4.cpp
@@ -12,35 +12,11 @@ struct Mahasiswa{
     int umur;
 };
 int main(){
-    Mahasiswa mhs[3];
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Data ke - " << (i+1) << " : " << endl;
-        cout << "Nomor Mahasiswa : ";
-        getline(cin, mhs[i].nim);
-        cout << "Nama Mahasiswa : ";
-        getline(cin, mhs[i].nama);
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    getline(cin,mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline(cin,mhs.nama);
 
-        cout << "Alamat Mahasiswa : " << endl;
-        cout << "\t Nama Desa : ";
-        cin >> mhs[i].alamat.desa;
-        cout << "\t Nama Kota : ";
-        cin >> mhs[i].alamat.kota;
-
-        cout << "Umur Mahasiswa : ";
-        cin >> mhs[i].umur;
-        cin.ignore(1, '\n');
-        cout << endl;
-    }
-      for (int i = 0; i < 3; i++){
-    cout << endl;
-    cout << "Data Mahasiswa ke - " << (i+1) << " : " << endl;
-    cout << "\n Nim : " << mhs[i].nim;
-    cout << "\n Nama : " << mhs[i].nama;
-    cout << "\n Alamat : ";
-    cout << "\n \t Desa : " << mhs[i].alamat.desa;
-    cout << "\n \t Kota : " << mhs[i].alamat.kota;
-    cout << "\n Umur : " << mhs[i].umur;
-    }
 }
 
