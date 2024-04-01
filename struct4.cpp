f81a24f8800515608264dccba14854b83495556e
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-01 15:37:07 +0700

    penggunaan for kedua untuk menampilkan data

--- struct4.cpp
+++ struct4.cpp
@@ -32,4 +32,15 @@ int main(){
         cin.ignore(1, '\n');
         cout << endl;
     }
+      for (int i = 0; i < 3; i++){
+    cout << endl;
+    cout << "Data Mahasiswa ke - " << (i+1) << " : " << endl;
+    cout << "\n Nim : " << mhs[i].nim;
+    cout << "\n Nama : " << mhs[i].nama;
+    cout << "\n Alamat : ";
+    cout << "\n \t Desa : " << mhs[i].alamat.desa;
+    cout << "\n \t Kota : " << mhs[i].alamat.kota;
+    cout << "\n Umur : " << mhs[i].umur;
+    }
 }
+
